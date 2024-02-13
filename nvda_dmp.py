e0fc2eda8db74c5e9d66401275e5cf1b62c674b5
Danstiv <50794055+Danstiv@users.noreply.github.com>
2024-02-13 18:32:51 +0700

    Add some debug logging

--- nvda_dmp.py
+++ nvda_dmp.py
@@ -18,22 +18,35 @@ import sys
 
 from fast_diff_match_patch import diff
 
+import time
+init_time = 1707572604.8901055
+def my_log(event):
+	with open('d:/temp/log.log', 'ab') as f:
+		f.write(f'{round(time.time()-init_time, 3)}\n{event}\n'.encode())
 
 def main():
     goodLineEndings = (b"\n", b"\r")
     while True:
+        my_log('reading text length')
         oldLen, newLen = struct.unpack("=II", sys.stdin.buffer.read(8))
+        my_log(f'read length, old: {oldLen}, new: {newLen}')
         if not oldLen and not newLen:
+            my_log('read zeros, terminating dmp')
             break  # sentinal value
+        my_log('Reading old text')
         oldText = sys.stdin.buffer.read(oldLen)
+        my_log('Reading new text')
         newText = sys.stdin.buffer.read(newLen)
         res = b""
+        my_log('Calculating diff')
         for op, text in diff(oldText, newText, counts_only=False):
             if op == "+":
                 res += text
                 if not text.endswith(goodLineEndings):
                     res += b"\n"
+        my_log('Writing diff size')
         sys.stdout.buffer.write(struct.pack("=I", len(res)))
+        my_log('Writing diff')
         sys.stdout.buffer.write(res)
         sys.stdin.flush()
         sys.stdout.flush()
